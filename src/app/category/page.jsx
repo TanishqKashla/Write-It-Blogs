e941534613ead12affac3677d0a216b37a1ec68a
TanishqKashla <tanishqkashla11@gmail.com>
2024-08-01 12:40:40 +0530

    Updated cateogry links

--- src/app/category/page.jsx
+++ src/app/category/page.jsx
@@ -1,17 +1,37 @@
+// import AllBlogs from "@/components/AllBlogs";
+
+// const BlogPage = ({ searchParams }) => {
+//     const { cat } = searchParams;
+
+
+//     return (
+//         <div className="mt-5">
+//             <div className="flex justify-center items-center mb-7">
+
+//                 <h1 className="text-6xl uppercase">{cat} Blog</h1>
+//             </div>
+//             <div >
+//                 <AllBlogs cat={cat} />
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default BlogPage;
+
+import { useRouter } from 'next/router';
 import AllBlogs from "@/components/AllBlogs";
 
-const BlogPage = ({ searchParams }) => {
-    const { cat } = searchParams;
-
+const BlogPage = () => {
+    const router = useRouter();
+    const { cat } = router.query;
 
     return (
         <div className="mt-5">
             <div className="flex justify-center items-center mb-7">
-
                 <h1 className="text-6xl uppercase">{cat} Blog</h1>
             </div>
-            <div >
-                <div>Cat to yaha bhi hai na?? {cat}</div>
+            <div>
                 <AllBlogs cat={cat} />
             </div>
         </div>
